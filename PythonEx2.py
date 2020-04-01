e655494b3249e0aafb2c9800fde62a5c0f7fb723
Beau Dinh <baod693@gmail.com>
2020-04-01 12:05:40 -0400

    ex2

---
+++ PythonEx2.py
@@ -0,0 +1,9 @@
+name = input ('What is your name?')
+welcome = 'hello ' + name + ' !'
+
+print (welcome.upper()) 
+
+length = len(name)
+welcome2 = 'your name has ' + str(length) + ' letters in it, awesome!'
+
+print (welcome2.upper()) 
