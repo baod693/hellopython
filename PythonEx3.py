cab0b2e53935cff12353b6b2796e3dc3d52e893d
Beau Dinh <baod693@gmail.com>
2020-04-01 12:39:10 -0400

    ex3

---
+++ PythonEx3.py
@@ -0,0 +1,7 @@
+print ('Please fill in the blanks bellow: \n __(blank)__\'s favorite subject in school is __(blank)__.')
+
+name = input ('What is your name? ')
+subject = input ('What is your favorite subject?' )
+
+answer = name + "'s favorite subject is " + subject
+print (answer)
